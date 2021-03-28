2f135e3b1a76cff1461b3e615bd2530a56ab96f5
Tiago Danin <TiagoDanin@outlook.com>
2021-03-28 17:34:33 -0300

    feat: create chat

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const helmet = require('helmet')
+const path = require('path')
+const {
+	v4: uuidv4
+} = require('uuid')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server)
+
+const port = process.env.PORT || 8000
+const build = 1
+
+app.use(helmet.contentSecurityPolicy())
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.expectCt())
+app.use(helmet.frameguard())
+app.use(helmet.hidePoweredBy())
+app.use(helmet.hsts())
+app.use(helmet.ieNoOpen())
+app.use(helmet.noSniff())
+app.use(helmet.permittedCrossDomainPolicies())
+app.use(helmet.referrerPolicy())
+app.use(helmet.xssFilter())
+
+app.set('trust proxy', 1)
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+io.on('connection', socket => {
+	console.log('[!] Join')
+
+	socket.on('disconnect', () => {
+		console.log('[!] Disconnect')
+	})
+
+	socket.on('sendMessage', message => {
+		const {
+			content
+		} = message
+		const messageId = uuidv4()
+		console.log('[+] sendMessage:', content, messageId)
+
+		socket.broadcast.emit('receiveMessage', {
+			content,
+			messageId
+		})
+	})
+})
+
+server.listen(port, () => console.log(`App listening at http://localhost:${port}`))
---
+++ public/chat.js
@@ -0,0 +1,62 @@
+const socketIo = io()
+const messageText = document.querySelector('#messageText')
+const buttonSend = document.querySelector('#buttonSend')
+const messageBox = document.querySelector('#messageBox')
+
+const templateMessageSender = message => {
+	const temporaryDiv = document.createElement('div')
+	temporaryDiv.innerHTML = `
+	<div class="me uk-grid-small uk-flex-bottom uk-flex-right uk-text-right" uk-grid>
+		<div class="uk-width-auto">
+			<div class="uk-card uk-card-body uk-card-small uk-card-primary uk-border-rounded">
+				<p class="uk-margin-remove" id="text"></p>
+			</div>
+		</div>
+			<div class="uk-width-auto">
+		</div>
+	</div>
+	`
+
+	const fragment = document.createDocumentFragment()
+	fragment.append(temporaryDiv)
+	fragment.querySelector('#text').textContent = message
+
+	return temporaryDiv
+}
+
+const templateMessageReceive = (message, id) => {
+	const temporaryDiv = document.createElement('div')
+	temporaryDiv.innerHTML = `
+	<div class="uk-card-body uk-padding-small">
+		<div class="guest uk-grid-small uk-flex-bottom uk-flex-left" uk-grid>
+			<div class="uk-width-auto">
+				<div class="uk-card uk-card-body uk-card-small uk-card-default uk-border-rounded">
+					<p class="uk-margin-remove" id="messageId:${id}"></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	`
+
+	const fragment = document.createDocumentFragment()
+	fragment.append(temporaryDiv)
+	fragment.getElementById(`messageId:${id}`).textContent = message
+
+	return temporaryDiv
+}
+
+messageBox.append(templateMessageReceive('Welcome!', 0))
+
+socketIo.on('receiveMessage', message => {
+	messageBox.append(templateMessageReceive(message.content, message.messageId))
+})
+
+buttonSend.addEventListener('click', () => {
+	if (messageText.value.length > 0) {
+		socketIo.emit('sendMessage', {
+			content: messageText.value
+		})
+		messageBox.append(templateMessageSender(messageText.value))
+		messageText.value = ''
+	}
+})
